5ab7108b959e480dae60b7eff631515a621c3d31
pasha29 <pasha4383@gmail.com>
2020-04-14 05:47:27 +0300

    Add hover effect

--- src/Redux/reduxStore.js
+++ src/Redux/reduxStore.js
@@ -1,12 +1,9 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-// import authReducer from './authReducer';
 import authReducer from './authReducer';
 
 let reducers = combineReducers({
-    auth: authReducer,
-    // products: productsReducer,
-    // product: productReducer
+    auth: authReducer
 });
 
 const store = createStore(reducers, applyMiddleware(thunkMiddleware))
