6b7a57360188e6538833b0c6b7035f14b7ea31b4
pasha29 <pasha4383@gmail.com>
2020-04-24 21:21:11 +0300

    Updates

---
+++ precache-manifest.c053d213c6ab452946af5ea8634ab643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c8acb23d6e49f6c027eca92153cd4e",
+    "url": "/listproducts/index.html"
+  },
+  {
+    "revision": "0bf143c700aa134c1f6b",
+    "url": "/listproducts/static/css/main.a6f09f7b.chunk.css"
+  },
+  {
+    "revision": "9038bb0ecd6aa7041e0d",
+    "url": "/listproducts/static/js/2.5357c11c.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/listproducts/static/js/2.5357c11c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf143c700aa134c1f6b",
+    "url": "/listproducts/static/js/main.1b1bb9d5.chunk.js"
+  },
+  {
+    "revision": "8ce8f305572cb9294bd4",
+    "url": "/listproducts/static/js/runtime-main.cdad3688.js"
+  }
+]);
